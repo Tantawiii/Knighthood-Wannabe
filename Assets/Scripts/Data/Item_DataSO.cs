47db4569db0976046c1e7f6c5e59c3d5766872f9
Omar Tantawy <o.tantawy.o@gmail.com>
2026-08-09 14:44:50 +0300

    feat: Add item and skill data structures, implement item pickup functionality, and update TagManager

---
+++ Assets/Scripts/Data/Item_DataSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "RPG Setup/Item Data/Material Item", fileName = "Material Data - ")]
+public class Item_DataSO : ScriptableObject
+{
+    public string itemName;
+    public Sprite itemIcon;
+    public ItemType itemType;
+}
